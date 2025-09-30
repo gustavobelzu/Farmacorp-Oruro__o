061ac6b032db46d38b6b95f8d5014eead68d5c5d
Gustavo Belzu <gustavo.belzu@sistemas.edu.bo>
2025-09-30 02:15:40 -0400

    modifico modulo

--- inventarios/apps.py
+++ inventarios/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class InventariosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'inventarios'   # <- este nombre debe coincidir con el nombre de la carpeta
+    name = 'inventarios'
