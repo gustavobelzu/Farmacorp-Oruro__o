e99f2c689b6a82d3ab4c191b345503a110e48aa1
Gustavo Belzu <gustavo.belzu@sistemas.edu.bo>
2025-09-30 02:22:28 -0400

    modifico modulo

--- inventarios/models.py
+++ inventarios/models.py
@@ -1,9 +1,19 @@
 from django.db import models
-from empleados.models import EncargadoAlmacen  # o quien sea responsable del inventario
+
+# Create your models here.
+from django.db import models
+from empleados.models import EncargadoInventario
 
 class Inventario(models.Model):
     cantidad = models.IntegerField()
     estado = models.CharField(max_length=50)
-    fecha_actualizacion = models.DateField()
+    fecha_actualizacion = models.DateField(auto_now=True)
     stock_minimo = models.IntegerField()
-    encargado = models.ForeignKey(EncargadoAlmacen, on_delete=models.CASCADE)
+    encargado = models.ForeignKey(
+        EncargadoInventario,
+        on_delete=models.CASCADE,  # Si se elimina el encargado, se elimina su inventario
+        related_name='inventarios'
+    )
+
+    def __str__(self):
+        return f"Inventario {self.id} - Encargado: {self.encargado.empleado.nombre}"
