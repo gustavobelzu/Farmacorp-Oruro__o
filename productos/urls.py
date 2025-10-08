1c56a00c82ce4f178f32fedb9a967ed2e491c2b1
Gustavo Belzu <gustavo.belzu@sistemas.edu.bo>
2025-10-07 23:38:22 -0400

    tarea realizando menu producto listado

--- productos/urls.py
+++ productos/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listar_productos, name='producto_listar'),
+    path('', views.listar_productos, name='producto_listar'),  # <-- lista productos
     path('crear/', views.crear_producto, name='crear_producto'),
     path('editar/<str:codigo_barra>/', views.editar_producto, name='editar_producto'),
     path('eliminar/<str:codigo_barra>/', views.eliminar_producto, name='eliminar_producto'),
 ]
-
