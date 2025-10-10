314dc6081f42bd1e03b4b54d499bb38373046897
Gustavo Belzu <gustavo.belzu@sistemas.edu.bo>
2025-10-10 00:35:04 -0400

    tarea bloquer codigo de barra al modificar

--- productos/forms.py
+++ productos/forms.py
@@ -1,3 +1,4 @@
+# productos/forms.py
 from django import forms
 from .models import Producto
 
@@ -8,3 +9,9 @@ class ProductoForm(forms.ModelForm):
         widgets = {
             'fecha_vencimiento': forms.DateInput(attrs={'type': 'date'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Si es edición (instance existe), bloqueamos el campo código_barra
+        if self.instance and self.instance.pk:
+            self.fields['codigo_barra'].widget.attrs['readonly'] = True
