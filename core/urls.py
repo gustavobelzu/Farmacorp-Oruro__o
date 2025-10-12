b64f2eb2fbc9d5739c53c8f5f707b0689cc35482
Gustavo Belzu <gustavo.belzu@sistemas.edu.bo>
2025-10-12 01:06:21 -0400

    arreglando bug login

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: redirect('login')),  # Redirige al login
+path('', lambda request: redirect('usuarios:login')),  # Redirige al login
     path('clientes/', include('clientes.urls', namespace='clientes')),  # ← namespace
     path('farmacia/', include('farmacia.urls')),
     path('usuarios/', include('usuarios.urls')),
