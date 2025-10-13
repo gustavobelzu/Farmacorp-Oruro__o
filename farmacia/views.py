79f0a2e77805d4d397802f7360652d98fe0125f1
Gustavo Belzu <gustavo.belzu@sistemas.edu.bo>
2025-10-13 00:38:24 -0400

    realizando mantenimiento a usuarios login

--- farmacia/views.py
+++ farmacia/views.py
@@ -10,7 +10,7 @@ from .forms import FarmaciaForm
 def login_required_custom(view_func):
     def wrapper(request, *args, **kwargs):
         if not request.session.get('usuario_id'):
-            return redirect('login')
+            return redirect('usuarios:login')
         return view_func(request, *args, **kwargs)
     return wrapper
 # ------------------------------------------
